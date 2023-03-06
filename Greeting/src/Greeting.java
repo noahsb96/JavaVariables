c333f2e7ecdf9b3ee7eca841e1927e466d3e0b0f
noahsb96 <noahsb96@yahoo.com>
2023-03-05 17:56:55 -0700

    Completed Java Variables

---
+++ Greeting/src/Greeting.java
@@ -0,0 +1,19 @@
+
+public class Greeting {
+
+	public static void main(String[] args) {
+		
+		String name;
+		name = "Noah";
+		
+		System.out.println(	"Hello " + name	);
+		System.out.println(	"Good Morning " + name	);
+		
+		name = "Amy";
+		
+		System.out.println(	"Good Afternoon " + name	);
+		System.out.println( "Goodbye " + name	);
+
+	}
+
+}
